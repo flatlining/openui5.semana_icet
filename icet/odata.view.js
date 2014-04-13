87e4c611559b47ce743ca222bc272bfaed807989
Matias Schertel <mschertel@gmail.com>
2014-04-13 14:20:12 -0300

    odata view with back button

--- icet/odata.view.js
+++ icet/odata.view.js
@@ -1,33 +1,29 @@
-sap.ui.jsview("icet.main", {
+sap.ui.jsview("icet.odata", {
 
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf icet.main
+	* @memberOf icet.odata
 	*/ 
 	getControllerName : function() {
-		return "icet.main";
+		return "icet.odata";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf icet.main
+	* @memberOf icet.odata
 	*/ 
 	createContent : function(oController) {
 		var i18nModel = new sap.ui.model.resource.ResourceModel({
 			bundleUrl: "i18n/messageBundle.properties"
 		});
 
-		var btnToOData = new sap.m.Button ({
-			text: "{i18n>OData}",
-			press: function() {
-				app.to("idodata");
-			}
-		})
-
 		var oPage = new sap.m.Page({
-			title: "{i18n>mainTitle}",
+			title: "{i18n>OData}",
+			showNavButton: true,
+			navButtonPress: function() {
+				app.back();
+			},
 			content: [
-				btnToOData,
 			]
 		});
 		oPage.setModel(i18nModel, "i18n");
